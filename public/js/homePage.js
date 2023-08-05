6416ecbe0dbbdd388e70805d82bd7caf4b213f63
Sidak <sidak@Windowslaptop.local>
2023-08-05 14:59:03 -0400

    alot of proggres but can tfind a way to add a transpartent 3js object

--- public/js/homePage.js
+++ public/js/homePage.js
@@ -5,54 +5,64 @@ let columns = 0,
     toggled = false;
 
 const toggle = () => {
-  toggled = !toggled;
-  
-  document.body.classList.toggle("toggled");
+    toggled = !toggled;
+
+    document.body.classList.toggle("toggled");
 }
 
 const handleOnClick = index => {
-  toggle();
-  
-  anime({
-    targets: ".tile",
-    opacity: toggled ? 0 : 1,
-    delay: anime.stagger(50, {
-      grid: [columns, rows],
-      from: index
-    })
-  });
+        toggle();
+
+    anime({
+        targets: ".tile",
+        opacity: toggled ? 0 : 1,
+        delay: anime.stagger(50, {
+            grid: [columns, rows],
+            from: index
+        })
+    }) 
 }
 
+const simulateRandomClick = () => {
+    const tiles = document.getElementsByClassName("tile");
+    if (tiles.length > 0) {
+        const randomIndex = Math.floor(Math.random() * tiles.length);
+        const randomTile = tiles[randomIndex];
+        randomTile.click(); // Simulate the click event on the random tile
+    }
+};
+
+
 const createTile = index => {
-  const tile = document.createElement("div");
-  
-  tile.classList.add("tile");
-  
-  tile.style.opacity = toggled ? 0 : 1;
-  
-  tile.onclick = e => handleOnClick(index);
-  
-  return tile;
+    const tile = document.createElement("div");
+
+    tile.classList.add("tile");
+
+    tile.style.opacity = toggled ? 0 : 1;
+
+    tile.onclick = e => handleOnClick(index);
+
+    return tile;
 }
 
 const createTiles = quantity => {
-  Array.from(Array(quantity)).map((tile, index) => {
-    wrapper.appendChild(createTile(index));
-  });
+    Array.from(Array(quantity)).map((tile, index) => {
+        wrapper.appendChild(createTile(index));
+    });
 }
 
 const createGrid = () => {
-  wrapper.innerHTML = "";
-  
-  const size = document.body.clientWidth > 800 ? 100 : 50;
-  
-  columns = Math.floor(document.body.clientWidth / size);
-  rows = Math.floor(document.body.clientHeight / size);
-  
-  wrapper.style.setProperty("--columns", columns);
-  wrapper.style.setProperty("--rows", rows);
-  
-  createTiles(columns * rows);
+    wrapper.innerHTML = "";
+
+    const size = document.body.clientWidth > 800 ? 100 : 50;
+
+    columns = Math.floor(document.body.clientWidth / size);
+    rows = Math.floor(document.body.clientHeight / size);
+
+    wrapper.style.setProperty("--columns", columns);
+    wrapper.style.setProperty("--rows", rows);
+
+    createTiles(columns * rows);
 }
 
 createGrid();
@@ -60,3 +70,13 @@ createGrid();
 window.onresize = () => createGrid();
 
 console.log("hi")
+
+function backgroundTiles() {
+    setTimeout(simulateRandomClick, 1000);
+    setTimeout(simulateRandomClick, 3500);
+}
+
+setTimeout(simulateRandomClick, 1000);
+setTimeout(simulateRandomClick, 3500);
+
+setInterval(backgroundTiles, 9000);
--- server/main.js
+++ server/main.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const { dirname } = require('path');
 const app = express();
 const port = 3000;
+const path = require("path")
+app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"../public/html/homePage.html"));
 });
 
 app.listen(port, () => {
